8d0571e75611aaf2ca2f426e35dfb3790abd2b24
Alexandre Gomes Serra <alexandre.gomes@infouniron.com>
2017-06-14 08:08:25 -0400

    Criado o metodo somar

--- ProjetoCalculadora/Classes/Classes/Metodos.cs
+++ ProjetoCalculadora/Classes/Classes/Metodos.cs
@@ -7,6 +7,15 @@ namespace Classes.Classes
 {
     public class Metodos
     {
-        
+        private double resultado;
+
+        public double Somar(double n1, double n2)
+        {
+            resultado = n1 + n2;
+
+            return resultado;
+        }
+
+
     }
 }
